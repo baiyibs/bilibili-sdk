cef9127ff765fa2b4c2e08e11a290893d0a818ea
baiyi <baiyibs@vip.qq.com>
2026-06-13 11:53:30 +0800

    fix: 修复依赖冲突

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:6.0.0"))
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("com.squareup.okhttp3:okhttp:5.4.0")
